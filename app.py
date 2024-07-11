ff8cb2f2727d1c2a0ddaa2980c7bde902874ba58
aa57c <aa57c@umsystem.edu>
2024-07-10 22:39:22 -0500

    added AES and PQC methods

--- app.py
+++ app.py
@@ -26,9 +26,8 @@ app = Flask(__name__)
 # host = "db"
 # port = "5432"
 
-'''
-# AES Encryption
 
+# AES Encryption
 def aes_encrypt(data, key):
     cipher = AES.new(key, AES.MODE_EAX)
     ciphertext, tag = cipher.encrypt_and_digest(data)
@@ -42,51 +41,39 @@ def aes_decrypt(encrypted_data, key):
     return cipher.decrypt_and_verify(ciphertext, tag).decode('utf-8')
 
 # PQC Key Encapsulation (Encryption)
-def pqc_encapsulate(aes_key):
-    kem = liboqs.KeyEncapsulation('Kyber512')
-    public_key = kem.generate_keypair()
-    ciphertext, shared_secret = kem.encap_secret(public_key)
-    return base64.b64encode(ciphertext).decode('utf-8'), base64.b64encode(public_key).decode('utf-8'), base64.b64encode(kem.export_secret_key()).decode('utf-8')
+def pqc_encapsulate(aes_key, algorithm):
+    public_key, secret_key = pqc.encryption.generate_keypair(algorithm)
+    encrypted_aes_key = pqc.encryption.encrypt(public_key, aes_key, algorithm)
+    return base64.b64encode(encrypted_aes_key).decode('utf-8'), base64.b64encode(public_key).decode('utf-8'), base64.b64encode(secret_key).decode('utf-8')
 
 # PQC Key Decapsulation (Decryption)
-def pqc_decapsulate(ciphertext, secret_key, public_key):
-    kem = liboqs.KeyEncapsulation('Kyber512', secret_key=base64.b64decode(secret_key))
-    kem.set_public_key(base64.b64decode(public_key))
-    shared_secret = kem.decap_secret(base64.b64decode(ciphertext))
-    return shared_secret
+def pqc_decapsulate(encrypted_aes_key, secret_key, algorithm):
+    decrypted_aes_key = pqc.encryption.decrypt(secret_key, base64.b64decode(encrypted_aes_key), algorithm)
+    return decrypted_aes_key
 
+
+
+
+'''
 
 # ECC Key Generation
 def generate_ecc_key():
-    # Generate ECC key
-    # sk = SigningKey.generate(curve=NIST384p)
-    # vk = sk.verifying_key
-    # return sk, vk
+    sk = SigningKey.generate(curve=NIST384p)
+    vk = sk.verifying_key
+    return sk, vk
 
 # ECC Signing
 def ecc_sign(data, sk):
-    # ECC signing
-    # signature = sk.sign(data.encode('utf-8'))
-    # return base64.b64encode(signature).decode('utf-8')
+    signature = sk.sign(data.encode('utf-8'))
+    return base64.b64encode(signature).decode('utf-8')
 
 # ECC Verification
 def ecc_verify(data, signature, vk):
-    # verification
-    #try:
-    #    vk.verify(base64.b64decode(signature), data.encode('utf-8'))
-    #    return True
-    #except:
-    #    return False
-
-# Example Post-Quantum Encryption function
-def pqc_encrypt(data):
-    # Implement the specific post-quantum encryption here
-    return data
-
-# Example Post-Quantum Decryption function
-def pqc_decrypt(data):
-    # Implement the specific post-quantum decryption here
-    return data
+    try:
+        vk.verify(base64.b64decode(signature), data.encode('utf-8'))
+        return True
+    except:
+        return False
 '''
 
 # Route to save text to the database
@@ -97,44 +84,65 @@ def save_text():
         
         text = request.form['text']
 
-        '''
-        # AES encryption
         aes_key = get_random_bytes(32) # AES-256
         aes_encrypted = aes_encrypt(text.encode('utf-8'), aes_key)
 
+        pqc_algorithm = 'Kyber512'
+
+        encrypted_aes_key, pqc_public_key, pqc_secret_key = pqc_encapsulate(aes_key, pqc_algorithm)
+
+        text_data = {
+            'aes_encrypted': aes_encrypted,
+            'encrypted_aes_key': encrypted_aes_key,
+            'pqc_public_key': pqc_public_key,
+            'pqc_secret_key': pqc_secret_key
+        }
+        save_string_to_postgres(json.dumps(text_data))
+        return redirect(url_for('retrieve_text'))
+
+        '''
+        # AES encryption
+        aes_key = get_random_bytes(32)  # AES-256
+        aes_encrypted = aes_encrypt(text.encode('utf-8'), aes_key)
+        
+        pqc_algorithm = 'Kyber512'
+
+        
         # PQC encryption of AES key
         pqc_ciphertext, pqc_public_key, pqc_secret_key = pqc_encapsulate(aes_key)
 
         # ECC key generation and signing
-        # sk, vk = generate_ecc_key()
-        # ecc_signature = ecc_sign(text, sk)
-        # PQC encryption (example)
-        # pqc_encrypted = pqc_encrypt(text)
-
+        sk, vk = generate_ecc_key()
+        ecc_signature = ecc_sign(text, sk)
+        
         # Prepare data to be saved
         text_data = {
             'aes_encrypted': aes_encrypted,
-            'pqc_ciphertext': pqc_ciphertext,
-            'pqc_public_key': pqc_public_key,
-            'pqc_secret_key': pqc_secret_key,
+            #'pqc_ciphertext': pqc_ciphertext,
+            #'pqc_public_key': pqc_public_key,
+            #'pqc_secret_key': pqc_secret_key,
             #'ecc_signature': ecc_signature,
             #'ecc_verifying_key': vk.to_string().hex(),
             #'pqc_encrypted': pqc_encrypted
         }
         '''
-        save_string_to_postgres(json.dumps(text))
-        
-        return redirect(url_for('retrieve_text'))
     return render_template('message_save.html')
 
 # Route to retrieve and display text from the database
 @app.route('/retrieve', methods=['GET'])
 def retrieve_text():
     
-    text = retrieve_string_from_postgres()
-    '''
+    text_data = json.loads(retrieve_string_from_postgres())
 
     aes_encrypted = text_data['aes_encrypted']
+    pqc_ciphertext = text_data['encrypted_aes_key']
+    pqc_public_key = text_data['pqc_public_key']
+    pqc_secret_key = text_data['pqc_secret_key']
+
+
+
+
+    '''
     pqc_ciphertext = text_data['pqc_ciphertext']
     pqc_public_key = text_data['pqc_public_key']
     pqc_secret_key = text_data['pqc_secret_key']
@@ -156,7 +164,7 @@ def retrieve_text():
     # pqc_decrypted = pqc_decrypt(pqc_encrypted)
     '''
 
-    return render_template('message_view.html', text=text)
+    return render_template('message_view.html', text=text_data)
 
 # Run the Flask application
 if __name__ == '__main__':
