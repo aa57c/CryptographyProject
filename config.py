279aaa68f9fc854070fa9a9b23e4f65004f836d6
aa57c <aa57c@umsystem.edu>
2025-04-12 21:36:11 -0500

    removed unnecessary files

--- config.py
+++
@@ -1,6 +0,0 @@
-# Database config file, will leave it in case we need it. Docker should take care of this now.
-# import os
-
-# class Config:
-#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
